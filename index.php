1dfb7d9a6b02fd83b3d69843a68d04047bcee585
Ian Marcel Penteado e Silva <ianmarcel@mailfence.com>
2025-06-20 14:19:30 -0300

    14:19:30 - 20/06/2025

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="shortcut icon" href="favicon.ico" image/x-icon">
+  <link rel="shortcut icon" href="assets/midia/images/special/FAVICON.ico" image/x-icon">
   <link rel="stylesheet" href="style.css?v=<?php echo time(); ?>">
   <title>Ian Marcel - Meu Portifólio</title>
 </head>
@@ -14,15 +14,15 @@
     <h2>🦺 Site em contrução. Volte em breve! 🦺</h2>
   </header>
   <main>
-    <video width="350px" height="350px" autoplay loop >
-      <source src="assets/midia/vídeos/under_construction.mp4" type="video/mp4">
-    </video>
+    <img src="assets/midia/images/under_construction.gif" alt="builders">
   </main>
   <footer>
     <div class="footer-links">
-      
-      <a href="https://github.com/Ian-Marcel">GitHub</a>
-      <a href="https://www.linkedin.com/in/ian-marcel-penteado/">LinkedIn</a></a>
+      <h3>Visite meu:</h3>
+      <div class="links">
+        <a href="https://github.com/Ian-Marcel">GitHub</a>
+        <a href="https://www.linkedin.com/in/ian-marcel-penteado/">LinkedIn</a>
+      </div>
     </div>
   </footer>
 
