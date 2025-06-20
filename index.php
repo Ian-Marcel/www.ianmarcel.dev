ba882990fdea7ad39c8259f9f88eae077700a306
Ian Marcel Penteado e Silva <ianmarcel@mailfence.com>
2025-06-19 22:56:34 -0300

    First commit!

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="shortcut icon" href="favicon.ico" image/x-icon">
+  <link rel="stylesheet" href="style.css?v=<?php echo time(); ?>">
+  <title>Ian Marcel - Meu Portifólio</title>
+</head>
+
+<body>
+  <header class="Harvest">
+    <h2>🦺 Site em contrução. Volte em breve! 🦺</h2>
+  </header>
+  <main>
+    <video width="350px" height="350px" autoplay>
+      <source src="assets/midia/vídeos/under_construction.mp4" type="video/mp4">
+    </video>
+  </main>
+  <footer>
+    <div class="footer-links">
+      
+      <a href="https://github.com/Ian-Marcel">GitHub</a>
+      <a href="https://www.linkedin.com/in/ian-marcel-penteado/">LinkedIn</a></a>
+    </div>
+  </footer>
+
+</body>
+
+</html>
