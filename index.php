acc44af39c450487e747e19d28996c86476dc38f
Ian <ianmarcel@mailfence.com>
2025-08-05 17:16:26 -0300

    17:16:26 - 05/08/2025

--- index.php
+++ index.php
@@ -1,31 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="shortcut icon" href="assets/midia/images/special/FAVICON.ico" image/x-icon">
-  <link rel="stylesheet" href="style.css?v=<?php echo time(); ?>">
-  <title>Ian Marcel - Meu Portifólio</title>
-</head>
-
-<body>
-  <header class="Harvest">
-    <h2>🦺 Site em contrução. Volte em breve! 🦺</h2>
-  </header>
-  <main>
-    <img src="assets/midia/images/under_construction.gif" alt="builders">
-  </main>
-  <footer >
-    <div class="footer-links">
-      <h3>Visite meu:</h3>
-      <div class="links">
-        <a href="https://github.com/Ian-Marcel">GitHub</a>
-        <a href="https://www.linkedin.com/in/ian-marcel-penteado/">LinkedIn</a>
-      </div>
-    </div>
-  </footer>
-
-</body>
-
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="shortcut icon" href="assets/midia/images/special/FAVICON.ico" image/x-icon >
+        <link rel="stylesheet" href="style.css?v=<?php echo time(); ?>">
+        <title>Ian Marcel - Meu Portifólio</title>
+    </head>
+    <body>
+        <header class="Harvest">
+            <h2>🦺 Site em contrução. Volte em breve! 🦺</h2>
+        </header>
+        <main>
+            <img src="assets/midia/images/under_construction.gif" alt="builders">
+        </main>
+        <footer >
+            <div class="footer-links">
+                <h3>Visite meu:</h3>
+                <div class="links">
+                    <a href="https://github.com/Ian-Marcel">GitHub</a>
+                    <a href="https://www.linkedin.com/in/ian-marcel-penteado/">LinkedIn</a>
+                </div>
+            </div>
+        </footer>
+    </body>
 </html>
